5ebb3c56d39fc364b0631d974cdee43f5e38f612
Alexander <alexander_petrenko@inbox.ru>
2020-06-24 20:10:05 +0300

    добавлены страницы 'news', 'about us' в views/, также прописаны Routes в web.php

---
+++ resources/views/about_us.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome</title>
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+            <div class="content">
+                <h1>
+                    About us
+                </h1>
+
+                <div class="about_us">
+                    <p>Phone: +78989989898</p>
+                    <p>Address: Long str., 99</p>
+                    <p>Email: shop_of_shops@shop_of_shops.ru</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Welcome</title>
+
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+            <div class="content">
+                <h1>
+                    Welcome page
+                </h1>
+
+                <div class="links">
+                    <a href="/news.php" style="margin-right: 20px;">News</a>
+                    <a href="/about_us.php">About us</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
